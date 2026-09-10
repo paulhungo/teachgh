e56d7f029fd509f89fb037158a75cde3f7bcccbb
Paul Hungo <paulhungo@users.noreply.github.com>
2026-09-10 22:14:58 +0000

    TeachGH v1.0 deploy (build 2)

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'tgh-v1';
+const ASSETS = ['index.html','data.json','manifest.json','version.json','icons/icon-192.png','icons/icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.match(e.request).then(old => c.put(e.request, copy)));
+      return r;
+    }).catch(() => caches.match(e.request).then(m => m || caches.match('index.html')))
+  );
+});
