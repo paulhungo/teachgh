095d53b3a7bb230abdd0a2c5ba0ba7139537c096
Paul Hungo <paulhungo@users.noreply.github.com>
2026-09-12 10:28:44 +0000

    TeachGH v1.5 (build 3): 37 life-skills modules, English + 10 subjects, Curriculum Docs page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tgh-v1';
+const CACHE = 'tgh-v2';
 const ASSETS = ['index.html','data.json','manifest.json','version.json','icons/icon-192.png','icons/icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
